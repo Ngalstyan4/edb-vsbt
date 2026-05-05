edf6b8a38f933bd9522723fa355147578ed0eb16
Narek Galstyan <narekg@berkeley.edu>
2026-05-05 16:39:20 -0400

    Add more threads in bench script

--- bench.sh
+++ bench.sh
@@ -1,10 +1,10 @@
-DATASET_LOCAL_DIR=/mnt/data/datasets/
-PGPASSWORD=postgres
+export DATASET_LOCAL_DIR=/mnt/data/datasets/
+export PGPASSWORD=postgres
 
 # bench openai 5m
 uv run python pgvector_suite.py -s config_v2/ivfflat_bq_rerank-5m-openai.yaml
 uv run python pgvector_suite.py -s config_v2/ivfflat_bq_rerank-5m-openai.yaml \
-  --skip-index-creation --query-clients 10
+  --skip-index-creation --query-clients 16
 uv run python vectorchord_suite.py -s config_v2/vectorchord_5m-openai.yaml
 uv run python vectorchord_suite.py -s config_v2/vectorchord_5m-openai.yaml \
-  --skip-index-creation --query-clients 10
+  --skip-index-creation --query-clients 16
